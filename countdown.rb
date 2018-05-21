d86e7775e3bbf45743a224b45833abb3aff966bf
Jason Mei <jason.mei403@gmail.com>
2018-05-21 22:06:08 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,4 +9,13 @@ def countdown(number)
   puts "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(number)
+  count = number
+  while count > 0
+    puts " #{count} SECOND(S)!"
+    sleep(1)
+    count -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
 countdown(12)
